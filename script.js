1b10eb7285654d40e31aff7811aab169b4fc7d25
Mrinal Raj <120112180+mrinalraj320@users.noreply.github.com>
2023-01-31 11:53:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+//add a eventListener at input box
+evaluatedText.addEventListener("input",calculateLength);
+
+function calculateLength() {
+    // console.log("someone wrote something");
+
+    //to get the word from the input box 
+
+    let word = evaluatedText.value;
+    let length = word.length;
+    letterCount.innerText = length;
+}
